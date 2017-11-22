9d1d61cb602775a508dbad1fb0315a08f7f29b7f
markballenger <mark.ballenger@e-hps.com>
2017-11-22 13:30:45 -0500

    Flyout.vue

---
+++ src/components/Flyout.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-navigation-drawer
+      temporary
+      
+      v-model="drawer"
+      light
+      permanent
+    >
+      <v-card>
+          <slot></slot>
+      </v-card>
+      
+    </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+    name: 'flyout',
+    data () {
+        return {
+            drawer: true
+        }
+    }
+}
+</script>
